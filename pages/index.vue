39faa759984033bbfc42d802a2439b4a9e14c103
RaniaZarka <raniazarka@hotmail.com>
2024-02-26 12:15:33 +0100

    NuxtLink added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-     <h2> Home</h2>
+<header>
+    <nav>
+
+        <!-- I am using NuxtLink to navigate through the app -->
+        <!-- By using NuxtLink, we can navigate through the components without sending a request to the server
+        While By useing the regular a tag <a href="/about"> About</a> for example, each time we navigate we will be sending a request to the server -->
+        <!-- Aslo by using NuxtLink some nuxt classes will be added automatically example if we navigate to about page, it will add autostyling to the html for this specific page -->
+     <NuxtLink to="/"> Nuxt app</NuxtLink>
+        <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+
+    </nav>
+</header>
+
+        <h2> Home</h2>
      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at semper purus. Vestibulum in est ullamcorper, luctus felis a, accumsan ipsum. Ut at mi libero. Integer non ipsum et mi gravida vehicula. Morbi accumsan metus et diam tempus vestibulum. Nulla aliquam fermentum augue, id posuere nunc malesuada in. Nulla facilisi. Proin aliquet libero ac feugiat condimentum. Quisque ut sollicitudin metus. Aenean dapibus ex vel risus euismod bib
  </p>
      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin eleifend, neque ac gravida sollicitudin, felis lacus lobortis sapien, in pharetra enim sem vel est. Aliquam tempor libero eget nulla dapibus efficitur. Mauris nec orci non sem laoreet consequat. Sed eget metus id velit congue venenatis vel vel nisi. Fusce suscipit lectus sit amet nulla hendrerit malesuada. Sed convallis, quam in posuere pellentesque, sapien ligula ultricies purus, at luctus odio est nec dui.
@@ -21,6 +38,6 @@ h2{
 
 p{
     margin:20px 0;
-    
+
 }
 </style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+   <p> Product details for {{ id }}</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at semper purus. Vestibulum in est ullamcorper, luctus felis a, accumsan ipsum. Ut at mi libero. Integer non ipsum et mi gravida vehicula. Morbi accumsan metus et diam tempus vestibulum. Nulla aliquam fermentum augue, id posuere nunc malesuada in. Nulla facilisi. Proin aliquet libero ac feugiat condimentum. Quisque ut sollicitudin metus. Aenean dapibus ex vel risus euismod bibendum. Ut condimentum velit nec neque lobortis varius. In nec consequat tortor. Nunc rhoncus orci ac augue commodo, non dictum velit viverra. </p> 
+  
+    </div>
+  </template>
+
+
+  <script setup>
+  // this will allow to get the id of the product that is the same as the id shown on the url
+  // the name if the const which is inside the {}, should match the same as the one inside the [] from the file name 
+
+const{id}= useRoute().params
+    </script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p{
+    margin:20px 0;
+    
+}
+</style>
