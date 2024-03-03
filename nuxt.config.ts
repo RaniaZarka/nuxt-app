9de867da0a9615adca6420d23c8393c0733dfef9
RaniaZarka <raniazarka@hotmail.com>
2024-03-03 19:04:00 +0100

    metadada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['@nuxtjs/tailwindcss']
+  modules:['@nuxtjs/tailwindcss'],
+
+  // here we can add all the metadata and description of the app and the stylesheet
+  app:{
+    head:{
+     title:'Nuxt Dojo',
+     meta:[
+      {name: 'description', content:'Everything about Nuxt'}
+     ],
+     link:[
+      {rel:'stylesheet', href:'https://fonts.googleapis.com/icon?family=Material+Icons'}
+     ]
+    }
+  }
 })
 
